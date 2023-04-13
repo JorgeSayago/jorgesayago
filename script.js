8c7006ded9a712404f2ec5f2d4ab43fca2ba2e92
JorgeSayago <130063995+JorgeSayago@users.noreply.github.com>
2023-04-13 08:17:49 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Hello!');
+
+var i=0;
+var elemento = document.getElementById("titulo1");
+elemento.addEventListener("click", function() {
+  i++// aumentar el valor de i cada vez que se hace clic
+  elemento.innerHTML = i; // actualizar el contenido del elemento con el valor actual de i
+})
+
+//var list = document.querySelector("#menu li")
+//console.log(list)
+
+//var list2 = document.querySelector("aside li:nith-child(even)")
+//console.log(list2)
